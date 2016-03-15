d740cdd5c67e2eb10b60d1d3fd46d0d4fbd2600c
Len Payne <len.payne@lambtoncollege.ca>
2016-03-15 14:02:27 -0400

    Initial Commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# jQuery, Angular and RESTful Web Services
+
+Some examples using jQuery, Angular and RESTful Web Services
+
+NOTE: May require an update to GlassFish to support javax.enterprise.context.*;
---
+++ src/java/todo/TodoService.java
@@ -0,0 +1,55 @@
+/*
+ * Copyright 2016 Len Payne <len.payne@lambtoncollege.ca>.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package todo;
+
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.json.Json;
+import javax.json.JsonArray;
+import javax.json.JsonArrayBuilder;
+import javax.json.JsonObject;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+
+/**
+ *
+ * @author Len Payne <len.payne@lambtoncollege.ca>
+ */
+@Path("todo")
+@ApplicationScoped // NOTE: This must be javax.enterprise.context.ApplicationScoped (and thus may not work on older versions of Glassfish)
+public class TodoService {
+    private TodoList todoList = new TodoList();
+    
+    @GET
+    @Produces("application/json")
+    public JsonArray getAll() {
+        JsonArrayBuilder json = Json.createArrayBuilder();
+        for (String todo : todoList.getTodoList()) {
+            json.add(todo);
+        }
+        return json.build();
+    }
+    
+    @POST
+    @Consumes("application/json")
+    public void add(JsonObject json) {
+        // Expects { "item": "some todoList entry" }      
+        todoList.add(json.getString("item"));
+    }
+}
